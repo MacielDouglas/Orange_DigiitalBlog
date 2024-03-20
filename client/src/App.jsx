a1234a136de298abaab138bb793ff2232c9bde42
MacielDouglas <douglasmaciel.dm@gmail.com>
2024-03-20 10:57:34 -0300

    Criado as paginas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,24 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Sobre from "./pages/Sobre";
+import Login from "./pages/Login";
+import Cadastro from "./pages/Cadastro";
+import Perfil from "./pages/Perfil";
+import Projetos from "./pages/Projetos";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sobre" element={<Sobre />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/perfil" element={<Perfil />} />
+          <Route path="/projetos" element={<Projetos />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
